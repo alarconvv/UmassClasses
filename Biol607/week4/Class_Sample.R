8951bed126fa7bd7b8e2181a35147e52c1d5fba9
alarconvv <alarconvv@gmail.com>
2023-09-26 17:35:34 -0400

    230926

---
+++ Biol607/week4/Class_Sample.R
@@ -0,0 +1,19 @@
+#-----------------
+# Sample
+#
+#
+#------------------
+
+
+# 1. What is a population you sample?
+#   
+# All Published papers in the last 10 years
+# that work with total evidence, 
+# molecular or morphological phylogenies (Reptiles).
+# 
+# 2. How do you ensure validity, reliability, 
+# and representativeness of a sample from your population?
+# 
+# I take care about they have DOI, they are registered in Web of sciences.
+
+
